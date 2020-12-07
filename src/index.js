bbbf7c207c91a3109a4ee4c4f44f31d25c7caec5
Matthew Machurek <mrjm2005@yahoo.com>
2020-12-07 12:05:32 -0600

    HistoryTraveler.js module is set up and ApplicationViews.js module is set up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import historyTraveler from './HistoryTraveler';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <historyTraveler />
   </React.StrictMode>,
   document.getElementById('root')
 );
