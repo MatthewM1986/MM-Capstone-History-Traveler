708cacb7e27150db9fd98eaa553cf53d4ffbdb41
Matthew Machurek <mrjm2005@yahoo.com>
2020-12-08 14:36:00 -0600

    added homepage welcome message

--- src/components/ApplicationView.js
+++ src/components/ApplicationView.js
@@ -4,6 +4,8 @@ import { Route } from "react-router-dom"
 export const ApplicationView = (props) => {
     return (
         <>
+            <h2>Begin Your History Travels Now!</h2>
+            <h3>Choose a Destination to Begin</h3>
         </>
     )
 }
