60acdf9f8f5b35c4dbe8e99ab46433e0c2faf5d8
Matthew Machurek <mrjm2005@yahoo.com>
2020-12-10 10:51:12 -0600

    re-ordered landmark single html

--- src/components/landmarks/LandmarkHTML.js
+++ src/components/landmarks/LandmarkHTML.js
@@ -4,13 +4,13 @@ import "./Landmark.css"
 export const LandmarksHTML = ({ landmarkObj }) => (
     <section className="landmark">
         <h3 className="landmark__name">{landmarkObj.name}</h3>
-        <div className="landmark__age">{landmarkObj.age}</div>
-        <div className="landmark__address">{landmarkObj.address}</div>
-        <div className="landmark__hours">{landmarkObj.hours}</div>
-        <div className="landmark__pricing">{landmarkObj.pricing}</div>
-        <div className="landmark__description">{landmarkObj.description}</div>
-        <div className="landmark__website">{landmarkObj.website}</div>
         <div className="landmark__image">{landmarkObj.imageURL}</div>
         <div className="landmark__type">{landmarkObj.type}</div>
+        <div className="landmark__age">{landmarkObj.age}</div>
+        <div className="landmark__description">{landmarkObj.description}</div>
+        <div className="landmark__hours">{landmarkObj.hours}</div>
+        <div className="landmark__pricing">{landmarkObj.pricing}</div>
+        <div className="landmark__address">{landmarkObj.address}</div>
+        <div className="landmark__website">{landmarkObj.website}</div>
     </section>
 )
