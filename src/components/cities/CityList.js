43b2f713e949011c27be611359a4fc06e967905d
Matthew Machurek <mrjm2005@yahoo.com>
2020-12-11 09:48:22 -0600

    citylist and city html is set up

---
+++ src/components/cities/CityList.js
@@ -0,0 +1,19 @@
+import React, { useContext, useEffect } from "react"
+import { CityContext } from "./CityProvider"
+import { CityHTML } from "./CityHTML"
+
+export const CityList = () => {
+    const { citiesArray, getCities } = useContext(CityContext)
+
+    useEffect(() => {
+        getCities()
+    }, [])
+
+    return (
+        <div className="cities">
+            {
+                citiesArray.map(c => < CityHTML key={c.id} cityObj={c} />)
+            })
+        </div>
+    )
+}
